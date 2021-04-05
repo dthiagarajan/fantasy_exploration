e9b840e6054399af730e5447f99b324a6c446ac0
Dilip Thiagarajan <dthiagarajan@github.com>
2021-04-05 15:00:26 -0400

    Update URL

--- app/app.py
+++ app/app.py
@@ -9,7 +9,7 @@ from fantasy.tasks.league import League
 
 @st.cache
 def get_file_content_as_string(path):
-    url = 'https://raw.githubusercontent.com/streamlit/demo-self-driving/master/' + path
+    url = 'https://raw.githubusercontent.com/dthiagarajan/fantasy-exploration/master/' + path
     response = urllib.request.urlopen(url)
     return response.read().decode("utf-8")
 
